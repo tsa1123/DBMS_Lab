83c086ffe4941533dc1bb5cfa38ae0cdfadc8c74
tsa1123 <abhijithkrishnanunnipzr@gmail.com>
2025-01-07 18:17:28 +0000

    Stage2

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -1,4 +1,41 @@
 #include "BlockBuffer.h"
-
+#include "../Disk_Class/Disk.h"
 #include <cstdlib>
 #include <cstring>
+
+BlockBuffer::BlockBuffer(int blockNum){
+	this->blockNum=blockNum;
+}
+
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum){}
+
+int BlockBuffer::getHeader(struct HeadInfo *head){
+	unsigned char buffer[BLOCK_SIZE];
+	Disk::readBlock(buffer, this->blockNum);
+	memcpy(&head->numSlots, buffer+24, 4);
+	memcpy(&head->numEntries, buffer+16, 4);
+	memcpy(&head->numAttrs, buffer+20, 4);
+	memcpy(&head->rblock, buffer+12, 4);
+	memcpy(&head->lblock, buffer+8, 4);
+
+	return SUCCESS;
+}
+
+int RecBuffer::getRecord(union Attribute *rec, int slotNum){
+	struct HeadInfo head;
+	this->getHeader(&head);
+	
+	int attrCount = head.numAttrs;
+	int slotCount = head.numSlots;
+
+	unsigned char buffer[BLOCK_SIZE];
+	Disk::readBlock(buffer, this->blockNum);
+	
+	int recordSize = attrCount * ATTR_SIZE;
+	unsigned char *slotPointer = buffer + HEADER_SIZE + slotCount + (recordSize * slotNum);
+	memcpy(rec, slotPointer, recordSize);
+
+	return SUCCESS;
+}
+
+
--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -2,29 +2,89 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
-#include<cstring>
-#include<iostream>
+#include "Buffer/BlockBuffer.h"
 using namespace std;
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
   Disk disk_run;
   // StaticBuffer buffer;
   // OpenRelTable cache;
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer,7777);
-  memcpy(buffer,"hello",6);
-  Disk::writeBlock(buffer,7777);
-  
-  unsigned char buffer2[BLOCK_SIZE];
-  Disk::readBlock(buffer2,7777);
-  cout<<buffer2<<endl;
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
 
-  for(int i=0;i<4;i++){
-  	Disk::readBlock(buffer2,i);
-	for(int j=0;j<BLOCK_SIZE;j++){
-		cout<<to_string(buffer2[j]);
+  HeadInfo relCatHeader,attrCatHeader;
+  relCatBuffer.getHeader(&relCatHeader);
+
+  for(int i=0;i<relCatHeader.numEntries;i++){
+  	Attribute relCatRecord[RELCAT_NO_ATTRS];
+	relCatBuffer.getRecord(relCatRecord,i);
+
+	printf("Relation: %s\n",relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+	
+	double numAttrs = relCatRecord[RELCAT_NO_ATTRIBUTES_INDEX].nVal;
+	Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+	int32_t attrCatBlockNo=ATTRCAT_BLOCK;
+
+	while(numAttrs>0){
+		RecBuffer attrCatBuffer(attrCatBlockNo);
+		attrCatBuffer.getHeader(&attrCatHeader);
+		
+		for(int k=0;k<attrCatHeader.numEntries;k++){
+			attrCatBuffer.getRecord(attrCatRecord,k);
+			if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal)==0){
+				const char* attrType=(attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal==NUMBER)? "NUM" : "STR";
+				printf(" %s: %s\n",attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,attrType);
+				numAttrs--;
+				if(numAttrs==0)break;
+			}
+		}
+		attrCatBlockNo=attrCatHeader.rblock;
 	}
-	cout<<endl;
+	printf("\n");
+  }
+
+  printf("Exercise 2:\n");
+  printf("============\n");
+  
+  Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+  int32_t attrCatBlockNo=ATTRCAT_BLOCK;
+  int flag=0;
+  while(attrCatBlockNo!=-1 && flag==0){
+	  RecBuffer attrCatBuffer(attrCatBlockNo);
+          attrCatBuffer.getHeader(&attrCatHeader);
+
+          for(int k=0;k<attrCatHeader.numEntries;k++){
+		  attrCatBuffer.getRecord(attrCatRecord,k);
+                  if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,"Students")==0){
+		  	if(strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"Class")==0){
+                        	unsigned char buffer[BLOCK_SIZE];
+				Disk::readBlock(buffer, attrCatBlockNo);
+
+				unsigned char* location = buffer + HEADER_SIZE + attrCatHeader.numSlots + (attrCatHeader.numAttrs * ATTR_SIZE * k) + (ATTRCAT_ATTR_NAME_INDEX * ATTR_SIZE);
+				memcpy(location, "Batch", 6);
+				Disk::writeBlock(buffer, attrCatBlockNo);
+				flag=1;
+				printf("[+]Schema Modified\n");
+				break;
+			}
+                  }
+          }
+          attrCatBlockNo=attrCatHeader.rblock;
+  }
+  if(flag==0)printf("[+]Attribute \'Class\' not Found!\n");
+
+  attrCatBlockNo=ATTRCAT_BLOCK;
+  while(attrCatBlockNo!=-1 ){
+          RecBuffer attrCatBuffer(attrCatBlockNo);
+          attrCatBuffer.getHeader(&attrCatHeader);
+                
+          for(int k=0;k<attrCatHeader.numEntries;k++){
+                  attrCatBuffer.getRecord(attrCatRecord,k);
+                  if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,"Students")==0){
+			  const char* attrType=(attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal==NUMBER)? "NUM" : "STR";
+                          printf(" %s: %s\n",attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,attrType);
+                  }
+          }
+          attrCatBlockNo=attrCatHeader.rblock;
   }
   //return FrontendInterface::handleFrontend(argc, argv);
 }
